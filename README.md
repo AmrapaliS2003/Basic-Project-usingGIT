0c7cb22a67a3329ff55345fff99106d373e26d51
AmrapaliS2003 <amrapali.cse@gmail.com>
2025-08-31 11:53:01 +0530

    added functionality

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Basic-Project-usingGIT
 This the basic project to know how git works.
+
+created by Amrapali Sonkamble
+
---
+++ app.js
@@ -0,0 +1,4 @@
+let button=document.querySelector("button");
+button.addEventListener('click',()=>{
+    return https;//en.wikipedia.org/wiki/Git;
+});
